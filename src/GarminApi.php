afc95ac854431e12205d12c9271dc169ab4e9b95
Mustapha <stoufa06@gmail.com>
2020-09-04 16:15:06 +0100

    Update package league/oauth1-client Update library to meet league/oauth1-client requirement and compatibilty. Documenting class propreties and methods

--- src/GarminApi.php
+++ src/GarminApi.php
@@ -1,17 +1,26 @@
 <?php
 namespace Stoufa\GarminApi;
 
-use League\Oauth1\Client\Credentials\TokenCredentials;
 use League\OAuth1\Client\Server\Server;
-use League\OAuth1\Client\Credentials\TemporaryCredentials;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\GuzzleException;
+use InvalidArgumentException;
+use League\OAuth1\Client\Credentials\CredentialsException;
 use League\OAuth1\Client\Credentials\CredentialsInterface;
+use League\OAuth1\Client\Credentials\TemporaryCredentials;
+use League\OAuth1\Client\Credentials\TokenCredentials;
 use League\OAuth1\Client\Server\User;
 
 class GarminApi extends Server
 {
-
+    /**
+     * Api connect endpoint
+     */
     const API_URL = "https://connectapi.garmin.com/";
+
+    /**
+     * Rest api endpoint
+     */
     const USER_API_URL = "https://healthapi.garmin.com/wellness-api/rest/";
 
     /**
@@ -19,7 +28,7 @@ class GarminApi extends Server
      *
      * @return string
      */
-    public function urlTemporaryCredentials()
+    public function urlTemporaryCredentials(): string
     {
         return self::API_URL . 'oauth-service/oauth/request_token';
     }
@@ -29,7 +38,7 @@ class GarminApi extends Server
      *
      * @return string
      */
-    public function urlAuthorization()
+    public function urlAuthorization(): string
     {
         return 'http://connect.garmin.com/oauthConfirm';
     }
@@ -39,7 +48,7 @@ class GarminApi extends Server
      *
      * @return string
      */
-    public function urlTokenCredentials()
+    public function urlTokenCredentials(): string
     {
         return self::API_URL . 'oauth-service/oauth/access_token';
     }
@@ -48,11 +57,10 @@ class GarminApi extends Server
      * Get the authorization URL by passing in the temporary credentials
      * identifier or an object instance.
      *
-     * @param TemporaryCredentials|string
-     *
+     * @param TemporaryCredentials|string $temporaryIdentifier
      * @return string
      */
-    public function getAuthorizationUrl($temporaryIdentifier)
+    public function getAuthorizationUrl($temporaryIdentifier): string
     {
         // Somebody can pass through an instance of temporary
         // credentials and we'll extract the identifier from there.
@@ -76,10 +84,12 @@ class GarminApi extends Server
      * @param TemporaryCredentials $temporaryCredentials
      * @param string $temporaryIdentifier
      * @param string $verifier
-     *
      * @return TokenCredentials
+     * @throws CredentialsException If a "bad response" is received by the server
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
      */
-    public function getTokenCredentials(TemporaryCredentials $temporaryCredentials, $temporaryIdentifier, $verifier)
+    public function getTokenCredentials(TemporaryCredentials $temporaryCredentials, string $temporaryIdentifier, string $verifier): TokenCredentials
     {
         if ($temporaryIdentifier !== $temporaryCredentials->getIdentifier()) {
             throw new \InvalidArgumentException(
@@ -100,13 +110,24 @@ class GarminApi extends Server
                 'form_params' => $bodyParameters
             ]);
         } catch (BadResponseException $e) {
-            return $this->handleTokenCredentialsBadResponse($e);
+            throw $this->getCredentialsExceptionForBadResponse($e, 'token credentials');
         }
         
         return $this->createTokenCredentials((string)$response->getBody());
     }
 
-    protected function protocolHeader($method, $uri, CredentialsInterface $credentials, array $bodyParameters = array())
+    /**
+     * Generate the OAuth protocol header for requests other than temporary
+     * credentials, based on the URI, method, given credentials & body query
+     * string.
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param CredentialsInterface $credentials
+     * @param array $bodyParameters
+     * @return string
+     */
+    protected function protocolHeader(string $method, string $uri, CredentialsInterface $credentials, array $bodyParameters = array()): string
     {
         $parameters = array_merge(
             $this->baseProtocolParameters(),
@@ -128,7 +149,15 @@ class GarminApi extends Server
         return $this->normalizeProtocolParameters($parameters);
     }
 
-    public function getActivitySummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * Get activity summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return string json response
+     * @throws Exception
+     */
+    public function getActivitySummary(TokenCredentials $tokenCredentials, array $params): string
     {
         $client = $this->createHttpClient();
         $query = http_build_query($params);
@@ -151,7 +180,15 @@ class GarminApi extends Server
         return $response->getBody()->getContents();
     }
 
-    public function getManuallyActivitySummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * get manually activity summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return string json response
+     * @throws Exception
+     */
+    public function getManuallyActivitySummary(TokenCredentials $tokenCredentials, array $params): string
     {
         $client = $this->createHttpClient();
         $query = http_build_query($params);
@@ -173,7 +210,15 @@ class GarminApi extends Server
         return $response->getBody()->getContents();
     }
 
-    public function getActivityDetailsSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * get activity details summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return string json response
+     * @throws Exception
+     */
+    public function getActivityDetailsSummary(TokenCredentials $tokenCredentials, array $params): string
     {
         $client = $this->createHttpClient();
         $query = http_build_query($params);
@@ -195,7 +240,17 @@ class GarminApi extends Server
         return $response->getBody()->getContents();
     }
     
-    public function backfill(TokenCredentials $tokenCredentials, string $uri, array $params) {
+    /**
+     * send request to back fill summary type
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param string $uri
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfill(TokenCredentials $tokenCredentials, string $uri, array $params): void
+    {
         $client = $this->createHttpClient();
         $query = http_build_query($params);
         $query = 'backfill/'.$uri.'?'.$query;
@@ -214,59 +269,149 @@ class GarminApi extends Server
                 "Received error [$body] with status code [$statusCode] when requesting historic $uri summary."
             );
         }
-        return $response->getBody()->getContents();
     }
 
-    public function backfillActivitySummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill activity summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillActivitySummary(TokenCredentials $tokenCredentials, array $params): void
     {
-        return $this->backfill($tokenCredentials, 'activities', $params);
+        $this->backfill($tokenCredentials, 'activities', $params);
     }
 
-    public function backfillDailySummary(TokenCredentials $tokenCredentials, array $params)
+     /**
+     * send request to back fill daily activity summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillDailySummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'dailies', $params);
     }
 
-    public function backfillEpochSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill daily epoch summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillEpochSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'epochs', $params);
     }
-    public function backfillActivityDetailsSummary(TokenCredentials $tokenCredentials, array $params)
+
+    /**
+     * send request to back fill activity details summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillActivityDetailsSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'activityDetails', $params);
     }
 
-    public function backfillSleepSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill sleep summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillSleepSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'sleep', $params);
     }
 
-    public function backfillBodyCompositionSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill body composition summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillBodyCompositionSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'bodyComps', $params);
     }
 
-    public function backfillStressDetailsSummary(TokenCredentials $tokenCredentials, array $params)
+
+    /**
+     * send request to back fill body composition summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillStressDetailsSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'stressDetails', $params);
     }
 
-    public function backfillUserMetricsSummary(TokenCredentials $tokenCredentials, array $params)
+
+    /**
+     * send request to back fill user metrics summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillUserMetricsSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'userMetrics', $params);
     }
 
-    public function backfillPulseOxSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill pulse ox summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillPulseOxSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'pulseOx', $params);
     }
 
-    public function backfillRespirationSummary(TokenCredentials $tokenCredentials, array $params)
+    /**
+     * send request to back fill respiration summary
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @param array $params
+     * @return void
+     * @throws Exception
+     */
+    public function backfillRespirationSummary(TokenCredentials $tokenCredentials, array $params): void
     {
         return $this->backfill($tokenCredentials, 'respiration', $params);
     }
 
-    public function deleteUserAccessToken(TokenCredentials $tokenCredentials) {
+    /**
+     * delete user access token: deregistration
+     *
+     * @param TokenCredentials $tokenCredentials
+     * @return void
+     * @throws Exception
+     */
+    public function deleteUserAccessToken(TokenCredentials $tokenCredentials): void
+    {
         $uri = 'user/registration';
         $client = $this->createHttpClient();
         $headers = $this->getHeaders($tokenCredentials, 'DELETE', self::USER_API_URL . $uri);
@@ -284,18 +429,26 @@ class GarminApi extends Server
                 "Received error [$body] with status code [$statusCode] when deleting user access token."
             );
         }
-        return true;
     }
 
-
-    
-
-    public function urlUserDetails()
+    /**
+     * returns user details url
+     *
+     * @return string
+     */
+    public function urlUserDetails(): string
     {
         return self::USER_API_URL . 'user/id';
     }
 
-    public function userDetails($data, TokenCredentials $tokenCredentials)
+    /**
+     * get user details: in garmin there is only user id
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return User
+     */
+    public function userDetails($data, TokenCredentials $tokenCredentials): User
     {
         $user = new User();
 
@@ -307,16 +460,39 @@ class GarminApi extends Server
         return $user;
     }
 
+    /**
+     * get user id
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     *  @return string|int|null
+     */
     public function userUid($data, TokenCredentials $tokenCredentials)
     {
-        return $data['userId'];
+        return isset($data['userId']) ? $data['userId'] : null;
     }
 
-    public function userEmail($data, TokenCredentials $tokenCredentials)
+    /**
+     * Left for compatibilty
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return string return empty string
+     */
+    public function userEmail($data, TokenCredentials $tokenCredentials): string
     {
+        return '';
     }
 
-    public function userScreenName($data, TokenCredentials $tokenCredentials)
+    /**
+     * Left for compatibilty
+     *
+     * @param mixed $data
+     * @param TokenCredentials $tokenCredentials
+     * @return string return empty string
+     */
+    public function userScreenName($data, TokenCredentials $tokenCredentials): string
     {
+        return '';
     }
 }
